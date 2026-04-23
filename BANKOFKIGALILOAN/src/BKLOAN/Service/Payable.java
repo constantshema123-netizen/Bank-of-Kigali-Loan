780e92074c04dfcf9bb32f5d15b7daffea2f5fc0
Constant Shema <constantshema123@gmail.com>
2026-04-23 21:40:39 +0200

    Add files via upload

---
+++ BANKOFKIGALILOAN/src/BKLOAN/Service/Payable.java
@@ -0,0 +1,11 @@
+package BKLOAN.Service;
+
+public interface Payable {
+
+    void processPayment(double amount);
+
+    double calculateRemainingBalance();
+
+    String generatePaymentReceipt();
+}
+
